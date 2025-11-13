719d60975f431e2252fecf4dc5590368a2814312
StreetwiseGamgee <cturner56@academic.rrc.ca>
2025-11-12 23:25:06 -0600

    Modified build screen attempting to implement means of fetching kernel version.
    Created 'ShellCmdletRepo' to house elevated requests. Modified PermReqHandler, and main activity
    
    Adjusted build.gradle and toml to redact a dependency

---
+++ app/src/main/java/com/cturner56/cooperative_demo_2/data/ShellCmdletRepo.kt
@@ -0,0 +1,28 @@
+package com.cturner56.cooperative_demo_2.data
+
+import android.content.pm.PackageManager
+import android.util.Log
+import rikka.shizuku.Shizuku
+import rikka.shizuku.ShizukuSystemProperties
+
+object ShellCmdletRepo {
+    fun getKernelVersion(): String {
+        try {
+            if (Shizuku.checkSelfPermission() != PackageManager.PERMISSION_GRANTED) {
+                Log.e("CIT - ShellCmdletRepo", "Permission has not been granted.")
+                return "Shizuku permission denied"
+            }
+
+            val kernelVersion = ShizukuSystemProperties.get("ro.kernel.version", "Kernel version not known")
+            return if (kernelVersion != "Kernel version not known" && kernelVersion.isNotBlank()) {
+                "Kernel version: $kernelVersion"
+            } else {
+                Log.w("CIT - ShellCmdletRepo", "ro.kernel.version property not found.")
+                "Kernel version not found"
+            }
+        } catch (e: Exception) {
+            Log.e("CIT - ShellCmdletRepo", "Error getting kernel version: ${e.message}")
+            return "Error getting kernel version"
+        }
+    }
+}
