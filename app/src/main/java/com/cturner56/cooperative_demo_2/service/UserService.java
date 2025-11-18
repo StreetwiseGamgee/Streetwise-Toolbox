aa0a5d36864f00fb35ea929fbdc37b74d7b5a9e1
StreetwiseGamgee <cturner56@academic.rrc.ca>
2025-11-18 17:29:20 -0600

    WIP: User Service Integration #2 added boilerplate code for implementing UserService
    Modified build.gradle.kts to accept aidl directory, and added such to the buildFeatures section
    
    Moved .aidl up one directory to match API example

---
+++ app/src/main/java/com/cturner56/cooperative_demo_2/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cturner56.cooperative_demo_2.service;
+
+import com.cturner56.cooperative_demo_2.IUserService;
+
+/*
+ * UserService:
+ * API-Ref:
+ * https://github.com/RikkaApps/Shizuku-API/blob/a27f6e4151ba7b39965ca47edb2bf0aeed7102e5/demo/src/main/java/rikka/shizuku/demo/service/UserService.java#L12
+ * Purpose:
+ *
+ */
+public class UserService extends IUserService.Stub {
+
+}
