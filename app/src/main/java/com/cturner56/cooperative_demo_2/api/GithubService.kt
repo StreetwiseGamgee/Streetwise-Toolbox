4f38350dd3a3848a51a0ea90d820093978043e91
StreetwiseGamgee <cturner56@academic.rrc.ca>
2025-11-26 21:37:38 -0600

    Modified: GithubService to add repo search endpoint, GithubViewModel to fetch a list of repos, and RepositoryScreen to display the list of repos

--- app/src/main/java/com/cturner56/cooperative_demo_2/api/GithubService.kt
+++ app/src/main/java/com/cturner56/cooperative_demo_2/api/GithubService.kt
@@ -5,15 +5,16 @@ import com.cturner56.cooperative_demo_2.api.model.RepositoryReleaseVersion
 import com.squareup.moshi.Json
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * Purpose - a data class which models the JSON response from Github's API repository search endpoint.
  * Acting as a wrapper for the list of spotlighted repositories.
  * @param repos || A list of [GithubRepository] objects.
- * @return An [RepositoriesQuery] object containing a list of retrieved repos.
+ * @return(s) A [RepositoriesResponse] object containing a list of retrieved repos.
  * doc-ref || "https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories
  */
-data class RepositoriesQuery(
+data class RepositoriesResponse(
     @Json(name = "repos")
     val repos: List<GithubRepository>
 )
@@ -26,7 +27,7 @@ interface GithubService {
      * Purpose - A function which retrieves details pertaining to a public Github repository.
      * @param owner || The username affiliated to the repo.
      * @param repo || The name of the repo.
-     * @return a [GithubRepository] object containing relevant repo details.
+     * @return(s) A [GithubRepository] object containing relevant repository details.
      * doc-ref || "https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository"
      */
     @GET("repos/{owner}/{repo}")
@@ -41,11 +42,23 @@ interface GithubService {
      * Purpose - A function which retrieves details pertaining to a repository's releases.
      * @param owner || The username affiliated to the repo.
      * @param repo || The name of the repo.
-     * @return a [List] of [RepositoryReleaseVersion] objects containing relevant release details.
+     * @return(s) A [List] of [RepositoryReleaseVersion] objects containing relevant release details.
+     * doc-ref: https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28
      */
     @GET("repos/{owner}/{repo}/releases")
     suspend fun getReleases(
         @Path("owner") owner: String,
         @Path("repo") repo: String
     ): List<RepositoryReleaseVersion>
+
+    /**
+     * Purpose - A function to search for repositories.
+     * @param query || The keywords used in searching.
+     * @return(s) a list of repositories contained inside the [RepositoriesResponse] object.
+     * doc-ref || https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories
+     */
+    @GET("search/repositories")
+    suspend fun queryRepositories(
+        @Query("q") query: String
+    ): RepositoriesResponse
 }
--- app/src/main/java/com/cturner56/cooperative_demo_2/viewmodel/GithubViewModel.kt
+++ app/src/main/java/com/cturner56/cooperative_demo_2/viewmodel/GithubViewModel.kt
@@ -11,6 +11,9 @@ import com.cturner56.cooperative_demo_2.api.model.RepositoryReleaseVersion
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
+import kotlin.collections.forEach
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 
 /**
  * Purpose - A ViewModel which exposes relevant repo information, and
@@ -18,50 +21,80 @@ import java.io.IOException
  */
 class GithubViewModel : ViewModel() {
 
-    // Holds the state for fetched repository details.
-    private val _repositoryState = mutableStateOf<GithubRepository?>(null)
-    val repositoryState: State<GithubRepository?> = _repositoryState
+    // Holds the state for the list of fetched repositories.
+    private val _repositoryListState = mutableStateOf<List<GithubRepository>>(emptyList())
+    val repositoryListState: State<List<GithubRepository>> = _repositoryListState
 
-    // Holds the state for fetched release details.
-    private val _releaseState = mutableStateOf<RepositoryReleaseVersion?>(null)
-    val releaseState: State<RepositoryReleaseVersion?> = _releaseState
+    // Holds the state for each repositories latest release.
+    private val _releasesState = mutableStateOf<Map<String, RepositoryReleaseVersion?>>(emptyMap())
+    val releasesState: State<Map<String, RepositoryReleaseVersion?>> = _releasesState
 
     // Holds the state for any error messages that could occur during retrieval.
     private val _errorState = mutableStateOf<String?>(null)
     val errorState: State<String?> = _errorState
 
+    private val featuredRepositories = listOf(
+        "ZG089/Re-Malwack",
+        "5ec1cff/TrickyStore",
+        "KOWX712/Tricky-Addon-Update-Target-List",
+        "KOWX712/PlayIntegrityFix",
+        "Dr-TSNG/ZygiskNext",
+        "j-hc/zygisk-detach",
+        "sidex15/susfs4ksu-module"
+    )
+
     /**
-     * Fetches latest repository information, and it's corresponding release data.
-     *
-     * The function launches a coroutine in the [viewModelScope].
-     * It makes async network calls to retrieve data, and updates the respective states
-     * [repositoryState] && [releaseState]. In the event of an error, it'll update [errorState]
+     * Purpose - Fetches a predefined list of featured repositories.
+     * The function launches coroutines to fetch both repository information and release details.
+     * for each repo listed inside [featuredRepositories].
      */
-    fun fetchRepoData(owner: String, repo: String) {
+    fun fetchFeaturedRepos() {
         viewModelScope.launch {
             try {
-                val repository = Api.retrofitService.getRepository(owner, repo)
-                _repositoryState.value = repository
-
-                // Fetch the list of releases, and safely take the first one (the most recent).
-                val releases = Api.retrofitService.getReleases(owner, repo)
-                _releaseState.value = releases.firstOrNull()
-
+                // Clears previous data, initiates loading.
+                _repositoryListState.value = emptyList()
+                _releasesState.value = emptyMap()
                 _errorState.value = null // Preventing previous errors from displaying
 
-                Log.i("CIT - GithubViewModel", "Data successfully fetched or $owner/$repo")
+                // fetch repository information
+                val repositoryRetrieval = featuredRepositories.map { fullName ->
+                    async {
+                        val (owner, repo) = fullName.split("/")
+                        Api.retrofitService.getRepository(owner, repo)
+                    }
+                }
+                val repositories = repositoryRetrieval.awaitAll()
+                _repositoryListState.value = repositories
+
+                // Fetch release information
+                val releaseRetrieval = repositories.map { repo ->
+                    async {
+                        val (owner, name) = repo.fullName.split("/")
+                        try{
+                            val releases = Api.retrofitService.getReleases(owner, name)
+                            // On success, returns a pair containing the name and release
+                            repo.fullName to releases.firstOrNull()
+                        } catch (e: HttpException) {
+                            Log.w("CIT - GithubViewModel", "No releases available for ${repo.fullName}", e)
+                            // On failure, returns a pair of the name and null.
+                            repo.fullName to null
+                        }
+                    }
+                }
+                _releasesState.value = releaseRetrieval.awaitAll().toMap()
 
             } catch (e: HttpException) {
+                //
                 _errorState.value = "Repository retrieval failed, please refresh application."
-                Log.e("CIT - GithubViewModel", "An API Err, ${e.message}")
-
+                Log.e("CIT - GithubViewModel", "An API Error has occurred, ${e.message}")
             } catch (e: IOException) {
                 // Handles network error output.
                 _errorState.value = "Network error has occurred, please connect to the internet"
                 Log.e("CIT - GithubViewModel", "Network error has occurred, ${e.message}")
             } catch (e: Exception) {
+                //
                 _errorState.value = "Sorry, we've hit a wall. Another error has occurred ${e.message}"
-                Log.e("CIT - GithubViewModel", "M'lord, we doth have an error which is most unexpected ${e.message}")
+                Log.e("CIT - GithubViewModel", "An unexpected error has occurred ${e.message}")
             }
         }
     }
